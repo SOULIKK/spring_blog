a541f2d8b2e7c4c67222ccb0a506bd5360201bf4
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-10-14 23:59:36 +0900

    FIX: PostDto, Controller typo

--- src/main/java/com/soulikk/spring_blog/dto/PostListDto.java
+++ src/main/java/com/soulikk/spring_blog/dto/PostListDto.java
@@ -1,23 +1,5 @@
 package com.soulikk.spring_blog.dto;
 
-
-import com.soulikk.spring_blog.service.PostService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/post")
-@Log4j2
-@RequiredArgsConstructor
 public class PostListDto {
 
-    private final PostService postService;
-
-    @GetMapping("/list")
-    public void getList() {
-
-    }
 }
