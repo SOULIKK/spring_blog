b6140b36b648894bd021c5ca2dde3868247194a9
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-11-19 17:09:54 +0900

    FEAT: Set file upload path

--- src/main/java/com/soulikk/spring_blog/SpringBlogApplication.java
+++ src/main/java/com/soulikk/spring_blog/SpringBlogApplication.java
@@ -2,6 +2,7 @@ package com.soulikk.spring_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +11,11 @@ public class SpringBlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBlogApplication.class, args);
+    }
+
+    @Bean(name = "uploadPath")
+    public String uploadPath() {
+        return "c:/upload/image/";
     }
 
 }
