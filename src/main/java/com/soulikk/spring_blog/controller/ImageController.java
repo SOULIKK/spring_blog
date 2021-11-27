f0f5ebcc235deced6f413805c480e81dde6251cb
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-11-27 23:11:37 +0900

    FEAT: Upload image for S3

--- src/main/java/com/soulikk/spring_blog/controller/ImageController.java
+++ src/main/java/com/soulikk/spring_blog/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.soulikk.spring_blog.controller;
 
+import com.soulikk.spring_blog.config.S3.S3Uploader;
 import com.soulikk.spring_blog.entity.Image;
 import com.soulikk.spring_blog.service.ImageService;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +9,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -24,11 +22,16 @@ public class ImageController {
 
     private final ImageService imageService;
 
+    private final S3Uploader s3Uploader;
+
 
     @PostMapping("/image")
     public ResponseEntity<?> imageUpload(@RequestParam("file") MultipartFile file) {
         try {
-            Image image = imageService.register(file);
+            String S3 = s3Uploader.upload(file, "static");
+            int end = S3.length();
+            S3 = "http://"+S3.substring(8, end);
+            Image image = imageService.register(S3);
             return ResponseEntity.ok().body("/image/"+ image.getImageId());
         } catch(Exception e) {
             e.printStackTrace();
@@ -41,12 +44,14 @@ public class ImageController {
     public ResponseEntity<?> getImage(@PathVariable Long imageId) {
         try {
             Image image = imageService.getImage(imageId);
-            Resource resource = resourceLoader.getResource("file:" + image.getFilePath());
+            Resource resource = resourceLoader.getResource(image.getFilePath());
             return ResponseEntity.ok().body(resource);
         } catch(Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().build();
         }
     }
+
+
 
 }
--- src/main/java/com/soulikk/spring_blog/service/ImageService.java
+++ src/main/java/com/soulikk/spring_blog/service/ImageService.java
@@ -4,6 +4,7 @@ import com.soulikk.spring_blog.entity.Image;
 import com.soulikk.spring_blog.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -33,41 +34,53 @@ public class ImageService {
         return imageRepository.findById(imageId).get();
     }
 
-
-    public Image register(MultipartFile file) throws Exception {
-
-        try {
-            if (file.isEmpty()) {
-                throw new Exception("존재하지 않는 파일");
-            }
-            String saveFileName = fileSave(uploadLocation.toString(), file);
-            Image saveFile = new Image();
-            saveFile.setFileName(file.getOriginalFilename());
-            saveFile.setSaveFileName(saveFileName);
-            saveFile.setContentType(file.getContentType());
-            saveFile.setSize(file.getResource().contentLength());
-            saveFile.setFilePath(uploadLocation.toString().replace(File.separatorChar, '/') +'/' + saveFileName);
-
-            imageRepository.save(saveFile);
-            return saveFile;
-
-        } catch(IOException e) {
-            throw new Exception("저장 실패", e);
-        }
-    }
+    // local
+//    public Image register(MultipartFile file) throws Exception {
+//
+//        try {
+//            if (file.isEmpty()) {
+//                throw new Exception("존재하지 않는 파일");
+//            }
+//            String saveFileName = fileSave(uploadLocation.toString(), file);
+//            Image saveFile = new Image();
+//            saveFile.setFileName(file.getOriginalFilename());
+//            saveFile.setSaveFileName(saveFileName);
+//            saveFile.setContentType(file.getContentType());
+//            saveFile.setSize(file.getResource().contentLength());
+//            saveFile.setFilePath(uploadLocation.toString().replace(File.separatorChar, '/') +'/' + saveFileName);
+//
+//            imageRepository.save(saveFile);
+//            return saveFile;
+//
+//        } catch(IOException e) {
+//            throw new Exception("저장 실패", e);
+//        }
+//    }
 
     public String fileSave(String uploadLocation, MultipartFile file) throws IOException {
         File uploadDir = new File(uploadLocation);
-
         if (!uploadDir.exists()) {
             uploadDir.mkdirs();
         }
-
         UUID uuid = UUID.randomUUID();
         String saveFileName = uuid.toString() + file.getOriginalFilename();
         File saveFile = new File(uploadLocation, saveFileName);
         FileCopyUtils.copy(file.getBytes(), saveFile);
-
         return saveFileName;
+    }
+
+    // s3
+    public Image register(String S3) {
+        Image saveFile = new Image();
+        saveFile.setSaveFileName(S3);
+        saveFile.setFilePath(S3);
+        imageRepository.save(saveFile);
+        return saveFile;
+    }
+
+
+    @Transactional
+    public void updateImage(Long postId, Long imgId) {
+        imageRepository.updatePostId(postId, imgId);
     }
 }
